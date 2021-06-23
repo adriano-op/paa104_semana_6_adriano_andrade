74cf4298f31ad695231d9a20d6aa296475715ab6
05701265684 <adriano.andrade@ufop.edu.br>
2021-06-23 06:36:07 -0300

    continuação da lista

--- main.cpp
+++ main.cpp
@@ -17,21 +17,20 @@
 using namespace std;
 
 
-//chessboard
-int board[100][100];
+//tem_ataque É uma função para verificar se a v[i, j] está sob ataque de alguma rainha.
+// Verifica se existe uma outra rainha na posição v[i,j] e nas diagonais.
+// Qualquer v[k, l] será diagonal à v[i, j], se k + l for igual a i + j ou k-l for igual a i-j.
 
-//std::vector<std::vector<int>> board;
-//function to check if the cell is attacked or not
-int is_attack(int i, int j, int N) {
+int tem_ataque(std::vector<std::vector<int>> &board, int i, int j, int tamanho_quadro) {
     int k, l;
-    //checking if there is a queen in row or column
-    for (k = 0; k < N; k++) {
+// verifica se há uma rainha na linha ou coluna
+    for (k = 0; k < tamanho_quadro; k++) {
         if ((board[i][k] == 1) || (board[k][j] == 1))
             return 1;
     }
-    //checking for diagonals
-    for (k = 0; k < N; k++) {
-        for (l = 0; l < N; l++) {
+// verificando as diagonais
+ for (k = 0; k < tamanho_quadro; k++) {
+        for (l = 0; l < tamanho_quadro; l++) {
             if (((k + l) == (i + j)) || ((k - l) == (i - j))) {
                 if (board[k][l] == 1)
                     return 1;
@@ -41,21 +40,20 @@ int is_attack(int i, int j, int N) {
     return 0;
 }
 
-int N_queen(int n) {
+int n_rainhas(std::vector<std::vector<int>> &board, int num_rainhas, int tamanho_quadro) {
     int i, j;
-    //if n is 0, solution found
-    if (n == 0)
+    // Caso base: se num_rainhas for 0, solução encontrada.
+    //Se não houver mais rainha, isso significa que todas as rainhas foram colocadas e temos uma solução.
+    if (num_rainhas == 0)
         return 1;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            //checking if we can place a queen here or not
-            //queen will not be placed if the place is being attacked
-            //or already occupied
-            if ((!is_attack(i, j, n)) && (board[i][j] != 1)) {
+    for (i = 0; i < tamanho_quadro; i++) {
+        for (j = 0; j < tamanho_quadro; j++) {
+            // verificando se podemos colocar uma rainha aqui ou não.
+            // a rainha não será colocada se o local ja estiver sendo atacado ou já ocupado
+            if ((!tem_ataque(board, i, j, tamanho_quadro)) && (board[i][j] != 1)) {
                 board[i][j] = 1;
-                //recursion
-                //wether we can put the next queen with this arrangment or not
-                if (N_queen(n - 1) == 1) {
+                // se podemos colocar a próxima rainha no tabuleiro
+                if (n_rainhas(board, num_rainhas - 1, tamanho_quadro) == 1) {
                     return 1;
                 }
                 board[i][j] = 0;
@@ -65,11 +63,11 @@ int N_queen(int n) {
     return 0;
 }
 
-// Aplica programação dinâmica para encontrar o número mínimo de moedas // de denominações d1 <d2 <... <dm
-// onde d1 = 1 que somam // um determinado montanten
-// Entrada: Número inteiro positivo e matrizD [1..m ] de números inteiros positivos crescentes
+// Aplica programação dinâmica para encontrar o número mínimo de moedas de denominações d1 <d2 <... <dm
+// onde d1 = 1 que somam um determinado montante n
+// Entrada: Número inteiro positivo e matriz [1..m ] de números inteiros positivos crescentes
 // indicando as denominações das moedas, onde D [1] = 1
-// Saída: O número mínimo de moedas que somam toneladas
+// Saída: O número mínimo de moedas
 int change_Making(int N, std::vector<int> m) {
     int M = m.size();
     int dp[N + 1];
@@ -92,15 +90,14 @@ int change_Making(int N, std::vector<int> m) {
 
 //exercicios 2-B)
 // fibonnaci recursivo, sem o uso de programação dinamica
-int fib(int n) {
+int fib_bottom_up(int n) {
     if (n == 0) {
         return 1;
     }
-
     if (n <= 2) {
         return 1;
     }
-    return fib(n - 1) + fib(n - 2);
+    return fib_bottom_up(n - 1) + fib_bottom_up(n - 2);
 
 }
 
@@ -111,14 +108,14 @@ int fibPD_bottom_up(int n) {
     res.push_back(0); //0 -> 1
     res.push_back(1); //1 -> 1
     for (int i = 2; i <= n; i++) {
-        res.push_back(fib(i - 1) + fib(i - 2));
+        res.push_back(fibPD_bottom_up(i - 1) + fibPD_bottom_up(i - 2));
     }
     return res[n];
 }
 
 
-int peso[5] = {0, 2, 1, 3, 2};
-int valor[5] = {0, 12, 10, 20, 15};
+std::vector<int> peso = {0, 2, 1, 3, 2};
+std::vector<int> valor = {0, 12, 10, 20, 15};
 int F[5][6];
 
 
@@ -154,28 +151,26 @@ int coinCollectionPD(int n, int m, std::vector<std::vector<int>> &C) {
 }
 
 
-
-
-int CoinCollecting_recursivo(int m, int n, std::vector<std::vector<int>> a) {
+int coinCollecting(int m, int n, std::vector<std::vector<int>> a) {
     int c[m][n];
     c[0][0] = a[0][0];
 
-    for( int i = 1; i<m; i++){
+    for (int i = 1; i < m; i++) {
         c[i][0] = c[i - 1][0] + a[i][0];
     }
-    for( int j = 1; j < n; j++){
+    for (int j = 1; j < n; j++) {
         c[0][j] = c[0][j - 1] + a[0][j];
     }
 
     for (int i = 1; i < m; i++) {
         for (int j = 1; j < n; j++) {
-                c[i][j] = max(c[i - 1][j], c[i][j - 1]) + a[i][j];
+            c[i][j] = max(c[i - 1][j], c[i][j - 1]) + a[i][j];
         }
     }
 
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++)
-            std::cout <<  c[i][j] << " ";
+            std::cout << c[i][j] << " ";
         std::cout << endl;
     }
     return c[m - 1][n - 1];
@@ -183,101 +178,108 @@ int CoinCollecting_recursivo(int m, int n, std::vector<std::vector<int>> a) {
 
 
 int main() {
+    // 1 a)   Para que tipo de problema serve a t ́ecnica de programação dinâmica.?
+    //PDF
 
+//    ---------------------------------------------------------------------------------------------------------------------
     // // 1 b)  Implemente  um  algoritmo  para  o  cálculo  do n-éesimo  número  de  Fibonacci  sem  utilizar programação dinâmica.
-//    int n = 3;
-//    std::cout << "Fibonacci Recursivo de " << n << ": " << fib(n) << std::endl;
+    //    int n = 3;
+    //    std::cout << "Fibonacci Recursivo de " << n << ": " << fib_bottom_up(n) << std::endl;
 
 
 //---------------------------------------------------------------------------------------------------------------------
 
-//    //1 c)  Implemente  um  algoritmo  para  o  cálculo  do n-éesimo  número  de  Fibonacci  com  utilizar programação dinâmica.
-//    // sequencia de fibinacci, com a utilziação de programação dinamica
-//    std::cout << "Fibonacci Programação Dinamica de " << n << ": " << fibPD_bottom_up(n) << std::endl;
+    //    //1 c)  Implemente  um  algoritmo  para  o  cálculo  do n-éesimo  número  de  Fibonacci  com  utilizar programação dinâmica.
+    //    // sequencia de fibinacci, com a utilziação de programação dinamica
+    //    std::cout << "Fibonacci Programação Dinamica de " << n << ": " << fibPD_bottom_up(n) << std::endl;
 
 //-----------------------------------------------------------------------------------------------------------------
-//    // 1d) Implemente um algoritmo para o problema do troco (Change-making problem(Seção 8.1)
-//    // utilizando programação dinâmica.
+    //    // 1d) Implemente um algoritmo para o problema do troco (Change-making problem(Seção 8.1)
+    //    // utilizando programação dinâmica.
 
-//    std::cout << "\n\n" << endl;
-//    std::cout << "Algoritmo para o problema do troco:" << endl;
-//    int n = 6;
-//    std::vector<int> v = {1, 3, 4};
-//    int coin = 6;
-//    int m = 3;
+    //    std::cout << "\n\n" << endl;
+    //    std::cout << "Algoritmo para o problema do troco:" << endl;
+    //    int n = 6;
+    //    std::vector<int> v = {1, 3, 4};
+    //    int coin = 6;
+    //    int m = 3;
 
-//    std::cout << "Quantidade de coin: " << n << endl;
-//    std::cout << "Coins Denominations: [ ";
-//    for (size_t i = 0; i < v.size(); i++) {
-//        std::cout << v[i] << ' ';
-//    }
-//    std::cout << "]" << endl;
-//    std::cout << "Num mínimo de moedas esperado: " << change_Making(n, v) << "\n";
+    //    std::cout << "Quantidade de coin: " << n << endl;
+    //    std::cout << "Coins Denominations: [ ";
+    //    for (size_t i = 0; i < v.size(); i++) {
+    //        std::cout << v[i] << ' ';
+    //    }
+    //    std::cout << "]" << endl;
+    //    std::cout << "Num mínimo de moedas esperado: " << change_Making(n, v) << "\n";
 
 //-----------------------------------------------------------------------------------------------------------------
 
-//    //1 e)  Implemente um algoritmo para o problema de coleta de moedas (Coin-collecting problem(Seção 8.1)
-//    // utilizando programação dinâmica.
-//
-//    std::vector<std::vector<int>> b =
-//            {{0, 1, 0, 1, 0, 0},
-//             {1, 0, 0, 1, 1, 0},
-//             {0, 1, 0, 1, 1, 0},
-//             {0, 0, 0, 1, 0, 1},
-//             {1, 1, 1, 0, 1, 0}};
-//    std::cout << "coinCollectionPD: " << coinCollectionPD(5, 6, b) << endl;
-//    std::cout << endl;
+    //    //1 e)  Implemente um algoritmo para o problema de coleta de moedas (Coin-collecting problem(Seção 8.1)
+    //    // utilizando programação dinâmica.
+    //
+    //    std::vector<std::vector<int>> b =
+    //            {{0, 1, 0, 1, 0, 0},
+    //             {1, 0, 0, 1, 1, 0},
+    //             {0, 1, 0, 1, 1, 0},
+    //             {0, 0, 0, 1, 0, 1},
+    //             {1, 1, 1, 0, 1, 0}};
+    //    std::cout << "coinCollectionPD: " << coinCollectionPD(5, 6, b) << endl;
+    //    std::cout << endl;
 
 //----------------------------------------------------------------------------------------------------------------------
 
-// 1 f)  Implemente um algoritmo para o problema de coleta de moedas (Coin-collecting problem(Se ̧c ̃ao 8.1))
-    // sem utilizar programação dinãmica.
+    // 1 f)  Implemente um algoritmo para o problema de coleta de moedas (Coin-collecting problem(Seção 8.1)) sem utilizar programação dinãmica.
 
-//    std::vector<std::vector<int>> a =
-//            {{0, 1, 0, 1, 0, 0},
-//             {1, 0, 0, 1, 1, 0},
-//             {0, 1, 0, 1, 1, 0},
-//             {0, 0, 0, 1, 0, 1},
-//             {1, 1, 1, 0, 1, 0}};
-//
-//    std::cout << "CoinCollecting: "<<endl ;
-//    std::cout <<"Num coins: " << CoinCollecting_recursivo(5, 6, a);
-//    std::cout << endl;
+    //    std::vector<std::vector<int>> a =
+    //            {{0, 1, 0, 1, 0, 0},
+    //             {1, 0, 0, 1, 1, 0},
+    //             {0, 1, 0, 1, 1, 0},
+    //             {0, 0, 0, 1, 0, 1},
+    //             {1, 1, 1, 0, 1, 0}};
+    //
+    //    std::cout << "CoinCollecting: "<<endl ;
+    //    std::cout <<"Num coins: " << coinCollecting(5, 6, a);
+    //    std::cout << endl;
 
-    //-----------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
     // 1 g) Implemente o algoritmo baseado em função de memória (memory function) para solução do problema da mochila (knapsack problem).
     // F -> armazena os valores
     // Variáveis globais peso[1..n], Value[1..n],
 
-//    for (int i = 1; i < 5; i++)
-//        for (int j = 1; j < 6; j++)
-//            F[i][j] = -1;
-//    std::cout <<"MFKnapsack: "<< MFKnapsack(4, 5);
+    //        for (int i = 1; i < 5; i++)
+    //            for (int j = 1; j < 6; j++)
+    //                F[i][j] = -1;
+    //        std::cout <<"MFKnapsack: "<< MFKnapsack(4, 5);
 
-//--------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+    // 2 (a)  Descreva a t ́ecnica de backtracking
+    // PDF
 
-//taking the value of N
-//    int N;
-//    std::cout << "Enter the value of N for NxN chessboard: ";
-//    std::cin >> N;
-//
-//    //inicializa o vetor com 0
-//    for (int i = 0; i < N; i++) {
-//        for (int j = 0; j < N; j++) {
-//            board[i][j] = 0;
-//        }
-//    }
-//
-//
-//
-//    //  N_queen(N);
-//
-//    //imprimir a  matix
-//    for (int i = 0; i < N; i++) {
-//        for (int j = 0; j < N; j++)
-//            std::cout << board[i][j] << " ";
-//        std::cout << endl;
-//    }
+// ----------------------------------------------------------------------------------------------------------------------
+    //2 (b)  Implemente um algoritmo baseado em backtracking para a a resolução do problema das n-rainhas.
+    //    int tamanho_quadro;
+    //    std::cout << "Insira o valor de num de rainhas (NxN): ";
+    //    std::cin >> tamanho_quadro;
+    //
+    //    std::vector<std::vector<int>> quadro(tamanho_quadro, std::vector<int>(tamanho_quadro));
+    //    //inicializando o tabuleiro com 0
+    //    for (int i = 0; i < tamanho_quadro; i++) {
+    //        for (int j = 0; j < tamanho_quadro; j++) {
+    //            quadro[i][j] = 0;
+    //        }
+    //    }
+    //    int qtidade_rainha = tamanho_quadro;
+    //    n_rainhas(quadro, qtidade_rainha, tamanho_quadro);
+    //    std::cout << "Número de rainhas:" << qtidade_rainha << endl;
+    //    std::cout << "Tamanho quadro:" << tamanho_quadro << endl;
+    //    std::cout << "Disposição: " << endl;
+    //
+    //    //imprimir a  matrix
+    //    for (int i = 0; i < tamanho_quadro; i++) {
+    //        for (int j = 0; j < tamanho_quadro; j++)
+    //            std::cout << quadro[i][j] << " ";
+    //        std::cout << endl;
+    //    }
 
 
     return 0;
